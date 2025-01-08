091792319f3419391807570e1feacb9ede7c979b
joshborseth <joshuaborseth@gmail.com>
2025-01-07 20:51:34 -0800

    test server only

--- src/app/dashboard/track/page.tsx
+++ src/app/dashboard/track/page.tsx
@@ -1,6 +1,7 @@
 import { Statements } from "@/components/statements/Statements";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
+import { Thing } from "@/components/thing";
 
 export default async function Page() {
   return (
@@ -19,6 +20,7 @@ export default async function Page() {
         <div className="grid w-full gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4"></div>
       </div>
       <Statements />
+      <Thing />
     </main>
   );
 }
