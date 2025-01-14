135534b40da30517ef9fbbc0d70fff505ed63491
joshborseth <joshuaborseth@gmail.com>
2025-01-13 20:55:34 -0800

    setup turso and update deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
-import { type Config } from "drizzle-kit";
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-import { env } from "@/env";
+config({ path: ".env" });
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  out: "./migrations",
+  dialect: "turso",
   dbCredentials: {
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_TOKEN,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_TOKEN!,
   },
-} satisfies Config;
+});
