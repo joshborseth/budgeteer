d9a4d4b5db2f1f22803dc90248d00dfbe07cdece
joshborseth <joshuaborseth@gmail.com>
2025-01-13 21:25:29 -0800

    update auth callback url

--- src/server/auth/google.ts
+++ src/server/auth/google.ts
@@ -4,5 +4,7 @@ import { Google } from "arctic";
 export const google = new Google(
   env.GOOGLE_CLIENT_ID,
   env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/login/google/callback",
+  env.NODE_ENV === "production"
+    ? "budgeteer-gamma.vercel.app/login/google/callback"
+    : "localhost:3000/login/google/callback",
 );
