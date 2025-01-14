b26570b5ec519ecde7c3fb841f4eb6dc645168d7
joshborseth <joshuaborseth@gmail.com>
2025-01-13 21:29:42 -0800

    add in https to callbacks

--- src/server/auth/google.ts
+++ src/server/auth/google.ts
@@ -5,6 +5,6 @@ export const google = new Google(
   env.GOOGLE_CLIENT_ID,
   env.GOOGLE_CLIENT_SECRET,
   env.NODE_ENV === "production"
-    ? "budgeteer-gamma.vercel.app/login/google/callback"
-    : "localhost:3000/login/google/callback",
+    ? "https://budgeteer-gamma.vercel.app/login/google/callback"
+    : "http://localhost:3000/login/google/callback",
 );
